bb8c12c6be429a230fe5f58e3d78ccdf7edba33e
Joan Llenas <joan.llenas.maso@gmail.com>
2018-04-06 21:18:17 +0200

    Removed the vertical-reverse mode

--- js/index.js
+++ js/index.js
@@ -6,7 +6,7 @@ var template = {
     labelRotation: 0
   },
   commit: {
-    spacingY: -50,
+    spacingY: -80,
     dot: {
       size: 14
     },
@@ -18,74 +18,77 @@ var template = {
 
 var gitgraph = new GitGraph({
   template: template,
-  author: "",
-  orientation: "vertical-reverse"
+  author: ""
 });
 
 
 
 // Start
-var master = gitgraph.branch("devFlow");
-master.commit({ message: "Start" });
+var master = gitgraph.branch("joanllenas");
+master.commit({ message: "HEAD" });
 
-// 2016
-var year = master.branch("2016");
-year.commit({ message: "Start" });
-var articles = year.branch("Article");
-var github = year.branch("GitHub");
-
-github.commit({ message: "Mar 22, 2016", detailId: "7guis-react" });
-github.commit({ message: "Aug 21, 2016", detailId: "7guis-elm" });
-github.commit({ message: "Sep 14, 2016", detailId: "videogular2" });
-
-articles.merge(year).delete();
-github.merge(year).delete();
-year.merge(master).delete();
-
-// 2017
-var year = master.branch("2017");
-year.commit({ message: "Start" });
-var articles = year.branch("Article");
-var github = year.branch("GitHub");
-
-github.commit({ message: "Feb 9, 2017", detailId: "flex-layout" });
-articles.commit({
-  message: "Mar 5, 2017",
-  detailId: "yet-another-typeScript-introduction"
-});
-github.commit({ message: "Apr 7, 2017", detailId: "elm-blog" });
-github.commit({ message: "May 21, 2017", detailId: "ngx-date-fns" });
-
-articles.merge(year).delete();
-github.merge(year).delete();
-year.merge(master).delete();
 
 // 2018
 var year = master.branch("2018");
-year.commit({ message: "Start" });
+year.commit({ message: "End of 2018" });
 var articles = year.branch("Article");
 var github = year.branch("GitHub");
 
+github.commit({ message: "Mar 11, 2018", detailId: "ts-data-maybe" });
 articles.commit({
-  message: "Jan 5, 2018",
-  detailId: "un-sorbo-de-tea-introduccion-a-elm-i"
+  message: "Feb 12, 2018",
+  detailId: "slaying-a-ui-antipattern-with-angular"
 });
 articles.commit({
   message: "Jan 6, 2018",
   detailId: "un-sorbo-de-tea-introduccion-a-elm-ii"
 });
 articles.commit({
-  message: "Feb 12, 2018",
-  detailId: "slaying-a-ui-antipattern-with-angular"
+  message: "Jan 5, 2018",
+  detailId: "un-sorbo-de-tea-introduccion-a-elm-i"
 });
-github.commit({ message: "Mar 11, 2018", detailId: "ts-data-maybe" });
 
 articles.merge(year).delete();
 github.merge(year).delete();
 year.merge(master).delete();
 
+
+// 2017
+var year = master.branch("2017");
+year.commit({ message: "End of 2017" });
+var articles = year.branch("Article");
+var github = year.branch("GitHub");
+
+github.commit({ message: "May 21, 2017", detailId: "ngx-date-fns" });
+github.commit({ message: "Apr 7, 2017", detailId: "elm-blog" });
+articles.commit({
+  message: "Mar 5, 2017",
+  detailId: "yet-another-typeScript-introduction"
+});
+github.commit({ message: "Feb 9, 2017", detailId: "flex-layout" });
+
+articles.merge(year).delete();
+github.merge(year).delete();
+year.merge(master).delete();
+
+
+// 2016
+var year = master.branch("2016");
+year.commit({ message: "End of 2016" });
+var articles = year.branch("Article");
+var github = year.branch("GitHub");
+
+github.commit({ message: "Sep 14, 2016", detailId: "videogular2" });
+github.commit({ message: "Aug 21, 2016", detailId: "7guis-elm" });
+github.commit({ message: "Mar 22, 2016", detailId: "7guis-react" });
+
+articles.merge(year).delete();
+github.merge(year).delete();
+year.merge(master).delete();
+
+
 // End
-master.commit("HEAD");
+master.commit("Start");
 
 setTimeout( function(){
   window.scrollTo();
