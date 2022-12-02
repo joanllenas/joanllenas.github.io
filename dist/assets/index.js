88da52e2f96a48e6ba5cab9c311c35b6b0605cc1
Joan Llenas Masó <joan.llenas.maso@gmail.com>
2022-12-02 02:49:30 +0100

    publish the dist folder, for now

---
+++ dist/assets/index.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+    document.querySelector('.theme-switcher')?.addEventListener('click', () => {
+        const theme = document.querySelector('html').dataset.theme;
+        document.querySelector('html').dataset.theme =
+            theme === 'dark' ? 'light' : 'dark';
+    });
+});
+//# sourceMappingURL=index.js.map
